21e8c2644ecc138261154f28e2d185622f355973
frank <wangsen9108@gmail.com>
2020-03-17 20:59:44 +0800

    对 axis 封装完成。

--- app/modules/components/d3/scale/component.ts
+++ app/modules/components/d3/scale/component.ts
@@ -1,11 +1,12 @@
 import Component from '@glimmer/component';
 // import D3Component from 'ember-d3-demo/utils/d3/d3Component';
 import { action } from '@ember/object';
-import { select } from 'd3-selection';
 import Layout from 'ember-d3-demo/utils/d3/layout';
 import AxisBuilder from 'ember-d3-demo/utils/d3/axis';
+import { timeMonth } from 'd3-time';
+import { timeFormat } from 'd3-time-format';
 
-interface D3ScaleArgs {}
+interface D3ScaleArgs { }
 
 export default class D3Scale extends Component<D3ScaleArgs> {
     @action
@@ -15,26 +16,17 @@ export default class D3Scale extends Component<D3ScaleArgs> {
 
         const pd = curLayout.getPadding();
         const svg = curLayout.getSvg()
-        const yAxisOpt = {
-            show: true,
-            className: 'y-axis',
-            girdIndex: 0,
-            position: 'left', // 'top'/'bottom'/left/right
-            offset: 0, 
-            type: 'category',
-            name: "",
-            min: 0,
-            max: 100
-        }
-        const yAxis = new AxisBuilder(curLayout,yAxisOpt)
+
         const xAxisOpt = {
             show: true,
             className: 'x-axis',
             girdIndex: 0,
             position: 'bottom', // 'top'/'bottom'/left/right
-            offset: 0, // 偏移量。 y 轴的偏移量比较容易预设，可为 x 轴的高度以及legend（如果是底部）
-                        // 的和。x 轴的偏移量最好是动态计算 y 轴的宽度。
-            type: 'category',
+            offset: 25.52,  // 偏移量。 y 轴的偏移量比较容易预设，可为 x 轴的高度以及
+            // legend（如果是底部）的和。
+            // 当先生成 y 轴的时候，offset 可以为0，会自动计算 y 轴宽度。
+            // x 轴的偏移量最好是动态计算 y 轴的宽度。
+            type: 'value',
             // 'value' 数值轴，适用于连续数据。
             // 'category' 类目轴，适用于离散的类目数据，为该类型时必须通过 data 设置类目数据。
             // 'time' 时间轴，适用于连续的时序数据，与数值轴相比时间轴带有时间的格式化，在刻度计算上也有所不同，例如会根据跨度的范围来决定使用月，星期，日还是小时范围的刻度。
@@ -44,25 +36,64 @@ export default class D3Scale extends Component<D3ScaleArgs> {
             min: 0,
             max: 100,
             splitNumber: 5,
+            data: null
         }
-        const xaAxis = new AxisBuilder(curLayout,xAxisOpt)
-
-    }
-    @action
-    initYAxis() {
-        const curLayout = new Layout('.bp-scale.y-linear');
-
+        const xaAxis = new AxisBuilder(curLayout, xAxisOpt)
         const yAxisOpt = {
             show: true,
             className: 'y-axis',
             girdIndex: 0,
             position: 'left', // 'top'/'bottom'/left/right
-            offset: 0, 
-            type: 'category',
+            offset: 19.11, // 对于 left / right offset 可以先设置为 0，在确定x轴的高度后，再修改为
+            // x 轴的高度，有底部 legend 的时候也要把legend 高度添加
+            type: 'value',
             name: "",
             min: 0,
             max: 100
         }
-        const axis = new AxisBuilder(curLayout,yAxisOpt)
+        const yAxis = new AxisBuilder(curLayout, yAxisOpt)
+    }
+    @action
+    initBand() {
+        const curLayout = new Layout('.bp-scale.band');
+
+        const yAxisOpt = {
+            show: true,
+            className: 'x-axis',
+            girdIndex: 0,
+            position: 'bottom', // 'top'/'bottom'/left/right
+            offset: 0,
+            type: 'category',
+            name: "",
+            min: 0,
+            max: 100,
+            data: ["2018Q1", "2018Q2", "2018Q3", "2018Q4", "2019Q1", "2019Q2", "2019Q3"]
+        }
+        const axis = new AxisBuilder(curLayout, yAxisOpt)
+    }
+    @action
+    initTime() {
+        const curLayout = new Layout('.bp-scale.band');
+        const svg = curLayout.getSvg()
+
+        const yAxisOpt = {
+            show: true,
+            className: 'x-axis',
+            girdIndex: 0,
+            position: 'bottom', // 'top'/'bottom'/left/right
+            offset: 0,
+            type: 'time',
+            name: "",
+            min: new Date(2015, 0, 1),
+            max: new Date(2016, 0, 1),
+            formatter: () => timeFormat('%y-%m')
+        }
+        const axisIns = new AxisBuilder(curLayout, yAxisOpt)
+        const xAxis = axisIns.getAxis()
+            .ticks(timeMonth.every(2))
+            .tickFormat(timeFormat('%y-%m'))
+
+        svg.select(`.${yAxisOpt.className}`)
+            .call(xAxis)
     }
 }
--- app/utils/d3/axis.ts
+++ app/utils/d3/axis.ts
@@ -1,24 +1,30 @@
-import { scaleLinear } from 'd3-scale';
+import { scaleLinear, scaleBand, scaleTime, scaleLog } from 'd3-scale';
 import { axisLeft, axisRight, axisBottom, axisTop, AxisDomain, Axis } from 'd3-axis';
 import Layout from './layout';
 import { getAxisSide } from './axisWidth'
 
 const DEFAULT = {
-    show: true,
-    className: '',
-    girdIndex: 0,
-    position: 'bottom', // 'top'/'bottom'
-    offset: 0, // 偏移量
-    type: 'category',
-    // 'value' 数值轴，适用于连续数据。
+    show: true, // 暂时无作用
+    className: '',  // 展示的坐标轴类名，TODO 默认添加 y-axis-l/y-axis-r/x-axis-t/x-axis-b
+    girdIndex: 0,   // 暂时无作用
+    position: 'bottom', // 'top'/'bottom'/‘left'/'right'
+    offset: 0, // 偏移量. y 轴的偏移量比较容易预设，可为 x 轴的高度以及
+    // legend（如果是底部）的和。
+    // 当先生成 y 轴的时候，offset 可以为 0，会自动计算 y 轴宽度。
+    // x 轴的偏移量最好是动态计算 y 轴的宽度。
+    type: 'value',
+    // 'value' 数值轴，适用于连续数据。 default
     // 'category' 类目轴，适用于离散的类目数据，为该类型时必须通过 data 设置类目数据。
     // 'time' 时间轴，适用于连续的时序数据，与数值轴相比时间轴带有时间的格式化，在刻度计算上也有所不同，例如会根据跨度的范围来决定使用月，星期，日还是小时范围的刻度。
     // 'log' 对数轴。适用于对数数据。
-    name: "",
+    name: "",   // 暂时无作用
     nameLocation: "end", // start / center / end
-    min: 0,
-    max: 0,
-    splitNumber: 5,
+    min: 0,     // 最小值。当type 为 time 时，应为 new Date()
+    max: 0,     // 最大值。
+    data: null, // category 必须
+    splitNumber: 5, // 坐标轴的分割段数，需要注意的是这个分割段数只是个预估值，最后实际显示的段数会在这个基础上根据分割后坐标轴刻度显示的易读程度作调整。
+    ticks: [5],     // ticks & formatter 暂不支持，可以在外部进行对坐标轴更新来格式化 ticks
+    formatter: null
 }
 class AxisBuilder {
     opt: any
@@ -31,10 +37,10 @@ class AxisBuilder {
         this.opt = { ...DEFAULT, ...opt };
         this.layout = layout
         switch (this.opt.type) {
-            case 'value':
-                console.log("value axis");
-                break;
             case 'category':
+                console.log("category axis");
+            // break;
+            case 'value':
             default:
                 this.createAxis();
                 break;
@@ -44,7 +50,7 @@ class AxisBuilder {
         let grid = this.layout.getGrid();
         let svg = this.layout.getSvg();
 
-        this.scale = this.rangeScale(this.opt.position, grid);
+        this.scale = this.rangeScale(this.opt, grid);
         this.axis = this.directionAxis(this.opt.position);
         svg.append('g')
             .classed(this.opt.className, true)
@@ -83,44 +89,77 @@ class AxisBuilder {
             .attr("transform", `translate(${distance[0]},${distance[1]})`);
 
     }
-    private rangeScale(direction: string, grid: any) {
+    private rangeScale(option: any, grid: any) {
         let range: number[] = [0, 0];
         let { padding, width, height } = grid;
-
-        switch (direction) {
+        let { offset, position, type, min, max, data } = option;
+        let domain: any[] = [min, max]
+        switch (position) {
             case 'bottom':
             case 'top':
                 let svg = this.layout.getSvg()
                 let yAxisWidth = getAxisSide(svg.select('.y-axis'))
-                range = [padding.pl, width - padding.pr - yAxisWidth]
+                range = [padding.pl + yAxisWidth + offset, width - padding.pr]
                 break;
             case 'right':
             case 'left':
             default:
-                range = [height - padding.pt, padding.pb + this.opt.offset]
+                range = [height - padding.pt - offset, padding.pb]
         }
-        return scaleLinear()
-            // TODO max & min 可以自行计算
-            .domain([this.opt.min, this.opt.max])
-            .range(range);
+        let scaleType = null
+
+        switch (type) {
+            case 'category':
+                scaleType = scaleBand()
+                    .domain(data)
+                    .range((<[number, number]>range))
+                break;
+            case 'time':
+                scaleType = scaleTime()
+                    .domain(domain)
+                    .range(range);
+                break;
+            case 'log':
+                scaleType = scaleLog();
+                break;
+            case 'value':
+            default:
+                // 数值轴，适用于连续数据
+                scaleType = scaleLinear()
+                    // TODO max & min 可以自行计算
+                    .domain(domain)
+                    .range(range);
+                break;
+        }
+        return scaleType
+        // TODO max & min 可以自行计算
+        // .domain([min, max])
+        // .range(range);
     }
     private directionAxis(direction: string): Axis<AxisDomain> {
-        // let axis: Axis<AxisDomain>
+        let axis: Axis<AxisDomain>
+        // let { formatter } = this.opt;
+        let scale = this.scale
+        // if (formatter instanceof Function) {
+        //     formatter = formatter()
+        // }
         switch (direction) {
             case 'right':
-                return axisRight(this.scale)
+                axis = axisRight(scale);
                 break;
             case 'bottom':
-                return axisBottom(this.scale)
+                axis = axisBottom(scale);
                 break;
             case 'top':
-                return axisTop(this.scale)
+                axis = axisTop(scale);
                 break;
             case 'left':
             default:
-                return axisLeft(this.scale)
+                axis = axisLeft(scale);
                 break;
         }
+        return axis;
+
     }
     public getScale() {
         return this.scale
@@ -128,9 +167,9 @@ class AxisBuilder {
     public getAxis() {
         return this.axis
     }
-    public getAxisWidth() {
-        return this.axisWidth
-    }
+    // public getAxisWidth() {
+    //     return this.axisWidth
+    // }
 }
 
 export default AxisBuilder
